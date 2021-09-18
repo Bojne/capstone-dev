b4b1d5e76cf64beb0a766a5a343ea31dd371e31d
bojne <bojne.john@gmail.com>
2021-09-18 13:36:55 +0200

    update project

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # capstone-dev
 A repo for capstone related development
 
+- Streamlit App: https://share.streamlit.io/bojne/capstone-dev/main/app/dev.py
+- Documentation: https://capstone22.netlify.app/
+
+---
 Yueh Han Hunag @ 2021
--- app/dev.py
+++ app/dev.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import librosa
+import librosa.display
 
+st.set_page_config(page_title="Capstone Dev",
+                   page_icon=":balloon:", layout="wide")
+st.header('Capstone Development')
 st.write('helloworld')
+
+
+def main():
+    st.sidebar.subheader("Home")
+    website_menu = st.sidebar.selectbox("Porject", ("Project 1", "Project 2"))
+
+
+if __name__ == '__main__':
+    main()
