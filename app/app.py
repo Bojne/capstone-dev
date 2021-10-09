307ddd833404f2202d67f79abe5b914b9dda099f
bojne <bojne.john@gmail.com>
2021-10-09 15:59:04 +0200

    refeactor python code for streamlit app

--- app/app.py
+++ app/app.py
@@ -12,38 +12,52 @@ st.set_page_config(page_title="Capstone Dev",
                    })
 
 
-def display_audio(audio_data, col=None, sr=44000):
-    x = librosa.load(audio_data, sr=None)
+def render_waveplot(x, sr):
+    st.write('`Waveplot`')
     fig = plt.figure(figsize=(14, 5))
-    plt.title('Spectrogram')
-    librosa.display.waveplot(x[0], sr=sr)
-    if col is not None:
-        col.pyplot(fig)
-    else:
-        st.pyplot(fig)
+    librosa.display.waveplot(x, sr=sr)
+    st.pyplot(fig)
 
 
-def spectrogram():
+def render_spectrogram(x, sr):
+    st.write('`Spectrogram`')
+    stft_x = librosa.stft(x)
+    fig = plt.figure(figsize=(14, 5))
+    Xdb = librosa.amplitude_to_db(abs(stft_x))
+    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+    plt.colorbar()
+    st.pyplot(fig)
+
+
+def render_chromagram(x, sr):
+    st.write('`Chromagram`')
+    fig = plt.figure(figsize=(14, 5))
+    chromagram = librosa.feature.chroma_stft(x, sr=sr)
+    librosa.display.specshow(chromagram, x_axis='time',
+                             y_axis='chroma', cmap='coolwarm')
+    plt.colorbar()
+    st.pyplot(fig)
+
+
+def display_audio(audio_data, col=None, sr=44000):
+    st.audio(audio_data)
+    x = librosa.load(audio_data, sr=None)[0]
+    render_waveplot(x, sr)
+    render_chromagram(x, sr)
+    render_spectrogram(x, sr)
+
+
+def spectrogram(audio):
     # x: numpy array
     X = librosa.stft(x[0])
     # converting into energy levels(dB)
-    Xdb = librosa.amplitude_to_db(abs(X))
-    fig2 = plt.figure(figsize=(20, 5))
-    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
-    plt.colorbar()
-    st.subheader("Spectrogram with normal axis")
-    st.pyplot(fig2)
+    X
 
 
 st.markdown(""" <style>
-#MainMenu {visibility: hidden;}
+# MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
-
-
-def plot_spectrogram(sr=44000
-                     ):
-    return None
 
 
 def analyzer_view():
@@ -60,23 +74,32 @@ def analyzer_view():
     if not model_audio:
         model_audio = 'src/audios/model.wav'
 
-    display_audio(user_audio, col1)
-    display_audio(model_audio, col2)
+    with col1:
+        display_audio(user_audio)
+
+    with col2:
+        display_audio(model_audio)
 
 
 def about_view():
     st.markdown(
         """
-        ## Welcome to the capstone!
+        # Welcome to the capstone!
 
-        ### Visit [Notion Home Page](https://yhhuang.notion.site/Capstone-Home-e9a1f458f2a143049100917f8f66272f)
-        
-        ### Sign up for project update / product waitlist 👇
+
+        # Sign up for project update / product waitlist 👇
         """
     )
     st.components.v1.iframe(
         src='https://6icg6d7rllb.typeform.com/to/yz5I5P1l',
         height=600)
+
+    st.markdown("""
+    # Visit [Notion Home Page](https://yhhuang.notion.site/Capstone-Home-e9a1f458f2a143049100917f8f66272f)
+
+    Yueh Han Huang
+    yhhuang@uni.minerva.edu
+    """)
 
 
 def main():
@@ -92,65 +115,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# path of the audio file
-audio_data = '../audios/model.wav'
-
-
-# st.header("Sample Audio Result")
-# # Get the audio display
-
-# # plotting the sampled signal
-
-
-# fig3 = plt.figure(figsize=(20, 5))
-# librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
-# plt.colorbar()
-# st.subheader("Spectrogram with log-scale axis")
-# st.pyplot(fig3)
-
-# chromagram = librosa.feature.chroma_stft(x[0], sr=sr)
-# fig4 = plt.figure(figsize=(15, 5))
-
-# librosa.display.specshow(chromagram, x_axis='time',
-#                          y_axis='chroma', cmap='coolwarm')
-# st.subheader("Chromagram")
-# st.pyplot(fig4)
-
-# st.header("User Recording Result")
-
-# # Get the audio display
-# y = librosa.load(audio_user, sr=sr)
-# fig = plt.figure(figsize=(14, 5))
-# # plotting the sampled signal
-# librosa.display.waveplot(y[0], sr=sr)
-# st.pyplot(fig)
-
-# # x: numpy array
-# Y = librosa.stft(y[0])
-# # converting into energy levels(dB)
-# Xdb = librosa.amplitude_to_db(abs(Y))
-
-# fig2 = plt.figure(figsize=(20, 5))
-# librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
-# plt.colorbar()
-# st.subheader("Spectrogram with normal axis")
-# st.pyplot(fig2)
-
-# fig3 = plt.figure(figsize=(20, 5))
-# librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
-# plt.colorbar()
-# st.subheader("Spectrogram with log-scale axis")
-# st.pyplot(fig3)
-
-# chromagram = librosa.feature.chroma_stft(y[0], sr=sr)
-# fig4 = plt.figure(figsize=(15, 5))
-
-# librosa.display.specshow(chromagram, x_axis='time',
-#                          y_axis='chroma', cmap='coolwarm')
-# st.subheader("Chromagram")
-# st.pyplot(fig4)
-
-
-col1, col2 = st.columns(2)
