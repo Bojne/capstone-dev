9423c6f854c8611ddcd7de52582ea5cb04325479
bojne <bojne.john@gmail.com>
2021-12-15 18:21:48 +0100

    add requirements

--- dev/get_audio.py
+++ dev/get_audio.py
@@ -6,6 +6,8 @@ from streamlit_bokeh_events import streamlit_bokeh_events
 stt_button = Button(label="Speak", width=100)
 
 stt_button.js_on_event("button_click", CustomJS(code="""
+
+    
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = true;
